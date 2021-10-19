1139534ae1cfd5683bee3974ded2d2096dfe0551
shahad alsayed <Shahad.Alsayed.jed.k1@tuwaiq.edu.sa>
2021-10-19 08:34:58 +0300

    done, fun

--- app/src/main/java/com/example/week4day2/MainActivity.kt
+++ app/src/main/java/com/example/week4day2/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.week4day2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnlogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnlogin=findViewById(R.id.btnLogIn)
+        btnlogin.setOnClickListener {
+            startActivity(Intent(this,MainActivity2::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/week4day2/MainActivity2.kt
+++ app/src/main/java/com/example/week4day2/MainActivity2.kt
@@ -1,11 +1,18 @@
 package com.example.week4day2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var txt:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        txt= findViewById(R.id.textView3)
+        txt.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
