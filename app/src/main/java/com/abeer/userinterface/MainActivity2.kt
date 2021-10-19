a890b93f6ee461ab9c91fbe03077395fa99a2380
Abeer <abeeralghamdi.jed.k1@tuwaiq.edu.sa>
2021-10-19 06:31:47 +0300

    HW 2 w4

---
+++ app/src/main/java/com/abeer/userinterface/MainActivity2.kt
@@ -0,0 +1,24 @@
+package com.abeer.userinterface
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var txtSignUp: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        txtSignUp=findViewById(R.id.txtSignUp)
+        txtSignUp.setOnClickListener {
+
+            val i = Intent(this,MainActivity()::class.java)
+            startActivity(i)
+            finish()
+
+
+        }
+    }
+}
