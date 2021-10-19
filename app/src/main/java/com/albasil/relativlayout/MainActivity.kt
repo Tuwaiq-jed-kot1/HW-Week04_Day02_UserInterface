0126f90edf1b6791fd89da86573a7f005778b6c4
Basil19 <bojsl-9@hotmail.com>
2021-10-19 09:21:22 +0300

    WEEK4-DAY3->DONE

--- app/src/main/java/com/albasil/relativlayout/MainActivity.kt
+++ app/src/main/java/com/albasil/relativlayout/MainActivity.kt
@@ -1,21 +1,45 @@
 package com.albasil.relativlayout
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnSnignUp:TextView
+    private lateinit var btnLogin:Button
+    private lateinit var editPassword:EditText
+    private lateinit var editUserName:EditText
+    private lateinit var login_facebook:LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        btnLogin=findViewById(R.id.id_login)
         btnSnignUp=findViewById(R.id.id_signUp)
+        editPassword=findViewById(R.id.editTextTextPassword)
+        editUserName=findViewById(R.id.editTextUser)
+
+        login_facebook=findViewById(R.id.id_login_facebook)
+
+        btnLogin.setOnClickListener {
+
+            if (editPassword.text.isNotEmpty() && editUserName.text.isNotEmpty()){
+
+                Toast.makeText(this, "Welcome ${editUserName.text}", Toast.LENGTH_SHORT).show()
+
+
+            }else{
+                Toast.makeText(this, "Please you must input user name or pass", Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
 
         btnSnignUp.setOnClickListener{
 
@@ -23,6 +47,26 @@ class MainActivity : AppCompatActivity() {
             startActivity(i)
             finish()
 
+        }
+
+
+
+        login_facebook.setOnClickListener{
+
+            startActivity(getOpenFacebookIntent());
+
+        }
+
+
+
+    }
+
+    private fun getOpenFacebookIntent(): Intent? {
+        return try {
+            packageManager.getPackageInfo("com.facebook.katana", 0)
+            Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/426253597411506"))
+        } catch (e: Exception) {
+            Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/appetizerandroid"))
         }
     }
 }
