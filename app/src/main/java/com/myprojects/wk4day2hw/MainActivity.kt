761406c51ec7f12719fadab4261122a23c64d2c9
musab <mas.gh123@hotmail.com>
2021-10-19 08:11:28 +0300

    hw

---
+++ app/src/main/java/com/myprojects/wk4day2hw/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.myprojects.wk4day2hw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
