c16bb8e34681d3fd425448c6efeeca27fd0fc88e
MahilaAlmutairi <Mahila.Almutairi.jed.k1@tuwaiq.edu.sa>
2021-10-19 08:41:29 +0300

    HW2_W4

--- app/src/main/java/com/mahila/Week04_Day02_UserInterface/logup.kt
+++ app/src/main/java/com/mahila/Week04_Day02_UserInterface/logup.kt
@@ -3,15 +3,16 @@ package com.mahila.Week04_Day02_UserInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class logup : AppCompatActivity() {
-    private lateinit var loginT: TextView
+    private lateinit var loginT: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logup)
-        loginT = findViewById(R.id.tv3)
+        loginT = findViewById(R.id.b11)
         loginT.setOnClickListener {
 
             val i = Intent(this, login::class.java)
